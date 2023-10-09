27320e4ebd2348fff7219581eb4d8a34829752e6
beecodeguy <beecodeguy@gmail.com>
2023-10-09 21:00:23 +0545

    search feature with params handling done

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,7 +1,23 @@
+import Filters from "@/components/Filters";
+import Header from "@/components/Header";
+import ResourceCard from "@/components/ResourceCard";
 import SearchForm from "@/components/SearchForm";
+import { getResources } from "@/sanity/actions";
 import React from "react";
 
-const Page = () => {
+export const revalidate = 900;
+
+interface Props {
+  searchParams: { [key: string]: string | undefined };
+}
+
+const Page = async ({ searchParams }: Props) => {
+  const resources = await getResources({
+    query: searchParams?.query || "",
+    category: searchParams?.category || "",
+    page: "1",
+  });
+
   return (
     <main className="flex-center paddings mx-auto w-full max-w-screen-2xl flex-col">
       <section className="nav-padding w-full">
@@ -12,7 +28,31 @@ const Page = () => {
         </div>
         <SearchForm />
       </section>
-      Filters
+      <Filters />
+      {(searchParams?.query || searchParams?.category) && (
+        <section className="flex-center mt-6 w-full flex-col sm:mt-20">
+          <Header
+            query={searchParams?.query || ""}
+            category={searchParams?.category || ""}
+          />
+          <div className="mt-12 flex w-full flex-wrap justify-center gap-16 sm:justify-start">
+            {resources?.length > 0 ? (
+              resources.map((resource: any) => (
+                <ResourceCard
+                  key={resource._id}
+                  title={resource.title}
+                  id={resource._id}
+                  image={resource.image}
+                  downloadNumber={resource.views}
+                  downloadLink={resource.downloadLink}
+                />
+              ))
+            ) : (
+              <p className="body-regular text-white-400">No resources found</p>
+            )}
+          </div>
+        </section>
+      )}
     </main>
   );
 };
